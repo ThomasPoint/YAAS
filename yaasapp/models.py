cb9ebc214d68e263c19995c94c15f47fe95d2b56
thomas <pointthomas84700@gmail.com>
2018-09-28 17:08:32 +0300

    add validation : email cannot be empty

--- yaasapp/models.py
+++ yaasapp/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
 from django.db.models.signals import post_save
@@ -12,6 +14,9 @@ class Profile(models.Model):
     location = models.CharField(max_length=40, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
+    def clean(self):
+        if not self.user.email:
+            raise ValidationError(_('The email should not be empty'))
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
